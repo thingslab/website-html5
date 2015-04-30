2b79443f36f25d5db5d445741d44d39ba76d2020
aziflaj <aldoziflaj95@gmail.com>
2015-04-30 12:36:08 -0700

    gulp uglifier

--- gulpfile.js
+++ gulpfile.js
@@ -2,13 +2,14 @@ var gulp = require('gulp'),
     autoprefixer = require('gulp-autoprefixer'),
     sass = require('gulp-sass'),
     jshint = require('gulp-jshint'),
-    minifyCss = require('gulp-minify-css');
+    minifyCss = require('gulp-minify-css'),
+    uglify = require('gulp-uglify');
 
 gulp.task('minify-css', function() {
     return gulp.src('assets/css/*.css')
         .pipe(autoprefixer())
         .pipe(minifyCss({compatibility: 'ie8'}))
-        .pipe(gulp.dest('public/css/*.min.css'));
+        .pipe(gulp.dest('dist/css'));
 });
 
 gulp.task('jshint', function() {
@@ -16,3 +17,15 @@ gulp.task('jshint', function() {
         .pipe(jshint())
         .pipe(jshint.reporter('default'));
 });
+
+gulp.task('uglify', function() {
+    return gulp.src('assets/js/*.js')
+        .pipe(uglify())
+        .pipe(gulp.dest('dist/js'));
+});
+
+gulp.task('default', [
+    'jshint',
+    'minify-css',
+    'uglify'
+]);
